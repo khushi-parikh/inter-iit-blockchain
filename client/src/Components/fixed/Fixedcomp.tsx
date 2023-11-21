778fe1baccf2053a1ac75e53d3c7820fb1f3c202
Ashutosh kumar <ashutoshkumar1jkl@gmail.com>
2023-11-21 15:29:35 +0530

    all setup and add page

---
+++ client/src/Components/fixed/Fixedcomp.tsx
@@ -0,0 +1,23 @@
+import React,{useState} from 'react'
+import Home from '../pages/Home'
+import TopNavbar from '../Navbar/TopNavbar'
+import SideNavbar from '../Navbar/SideNavbar'
+
+const Fixedcomp:React.FC = () => {
+    const [sidenav,setSidenav] = useState(true)
+  const [text,setText] = useState('close')
+  function handleclose(){
+    setSidenav(!sidenav);
+    setText(sidenav ? 'close' :   'open');
+}
+  return (
+    <div>
+        {/* <Home/> */}
+        <SideNavbar sidenav={sidenav} text={text} handleclose={handleclose} />
+        <TopNavbar sidenav={sidenav}/>
+
+    </div>
+  )
+}
+
+export default Fixedcomp
