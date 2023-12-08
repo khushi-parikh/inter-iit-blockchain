9b722fcef3084ff9f2ad5bb5d0af81338c58c21f
Arun <arunraghavssreeragam@gmail.com>
2023-12-09 02:59:34 +0530

    completed ipfs upload function

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,7 +13,7 @@ import Fixedcomp from './Components/fixed/Fixedcomp';
 import SongPage from './Components/Music/SongPage';
 import PlayList from './Components/Music/PlayList';
 import Upload from './Components/pages/Upload';
-import Appdemo from './Components/demo-music/Appdemo';
+// import Appdemo from './Components/demo-music/Appdemo';
 
 
 function App() {
@@ -30,7 +30,7 @@ function App() {
       <Route path="/profile" element={<Profile/>} />
       <Route path="/Playlist" element={<PlayList PlaylistName={''} NumOfSongs={0}/>} />
       <Route path="/upload" element={<Upload/>} />
-      <Route path="/App" element={<Appdemo/>} />
+      {/* <Route path="/App" element={<Appdemo/>} /> */}
 
 
       
